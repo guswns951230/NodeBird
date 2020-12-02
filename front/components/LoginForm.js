ef10971aeb636c84dd4ce981ed77b2d3bfd00944
guswns951230 <rkdguswns0803@gmail.com>
2020-12-03 00:00:28 +0900

    회원가입 페이지 만들기(커스텀 훅)

--- front/components/LoginForm.js
+++ front/components/LoginForm.js
@@ -2,6 +2,9 @@ import React, { useState, useCallback } from 'react';
 import { Form, Input, Button } from 'antd';
 import Link from 'next/link';
 import styled from 'styled-components';
+import PropTypes from 'prop-types';
+
+import useInput from '../hooks/useInput';
 
 const FormWrapper = styled(Form)`
     padding: 10px;
@@ -11,16 +14,8 @@ const ButtonWrapper = styled.div`
 `;
 
 const LoginForm = ({ setIsLoggedIn }) => {
-    const [id, setId] = useState('');
-    const [password, setPassword] = useState('');
-
-    const onChangeId = useCallback((e) => {
-        setId(e.target.value);
-    }, []);
-
-    const onChangePassword = useCallback((e) => {
-        setPassword(e.target.value);
-    }, []);
+    const [id, onChangeId] = useInput('');
+    const [password, onChangePassword] = useInput('');
 
     const onSubmitForm = useCallback(() => {
         console.log(id, password);
@@ -51,6 +46,10 @@ const LoginForm = ({ setIsLoggedIn }) => {
             </ButtonWrapper>
         </FormWrapper>
     );
+};
+
+LoginForm.propTypes = {
+    setIsLoggedIn: PropTypes.func.isRequired,
 };
 
 export default LoginForm;
