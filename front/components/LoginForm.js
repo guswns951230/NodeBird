117c9481542d9cec65f2ba04747d9b296de31675
guswns951230 <rkdguswns0803@gmail.com>
2020-12-02 16:57:34 +0900

    리렌더링 이해하기

--- front/components/LoginForm.js
+++ front/components/LoginForm.js
@@ -1,6 +1,14 @@
 import React, { useState, useCallback } from 'react';
 import { Form, Input, Button } from 'antd';
 import Link from 'next/link';
+import styled from 'styled-components';
+
+const FormWrapper = styled.div`
+    margin-left: 5px;
+`;
+const ButtonWrapper = styled.div`
+    margin-top: 10px;
+`;
 
 const LoginForm = () => {
     const [id, setId] = useState('');
@@ -15,28 +23,31 @@ const LoginForm = () => {
     }, []);
 
     return (
-        <Form style={{ marginLeft: 5 }}>
-            <div style={{ marginTop: 10 }}>
-                <label htmlFor="user-id">아이디</label>
-                <br />
-                <Input name="user-id" value={id} onChange={onChangeId} required />
-            </div>
-            <div style={{ marginTop: 10 }}>
-                <label htmlFor="user-password">비밀번호</label>
-                <br />
-                <Input
-                    name="user-password"
-                    type="password"
-                    value={password}
-                    onChange={onChangePassword}
-                    required
-                />
-            </div>
-            <div style={{ marginTop: 10 }}>
-                <Button type="primary" htmlType="submit" loading={false}>로그인</Button>
-                <Link href="/signup"><a><Button style={{ marginLeft: 5 }}>회원가입</Button></a></Link>
-            </div>
-        </Form>
+        <FormWrapper>
+            <Form>
+                <div>
+                    <label htmlFor="user-id">아이디</label>
+                    <br />
+                    <Input name="user-id" value={id} onChange={onChangeId} required />
+                </div>
+                <div>
+                    <label htmlFor="user-password">비밀번호</label>
+                    <br />
+                    <Input
+                        name="user-password"
+                        type="password"
+                        value={password}
+                        onChange={onChangePassword}
+                        required
+                    />
+                </div>
+                <ButtonWrapper>
+                    <Button type="primary" htmlType="submit" loading={false}>로그인</Button>
+                    <Link href="/signup"><a><Button style={{ marginLeft: 5 }}>회원가입</Button></a></Link>
+                </ButtonWrapper>
+            </Form>
+        </FormWrapper>
+
     );
 }
 
