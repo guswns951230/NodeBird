047ed9ee8a3b2f33423d813526ef8bad8158d748
guswns951230 <rkdguswns0803@gmail.com>
2020-12-05 21:45:53 +0900

    Image Carousel 구현하기(react-slick)

--- front/components/PostImages.js
+++ front/components/PostImages.js
@@ -2,16 +2,23 @@ import React, { useState, useCallback } from 'react';
 import PropTypes from 'prop-types';
 import { PlusOutlined } from '@ant-design/icons';
 
+import ImagesZoom from './ImagesZoom';
+
 const PostImages = ({ images }) => {
     const [showImagesZoom, setShowImagesZoom] = useState(false);
 
     const onZoom = useCallback(() => {
         setShowImagesZoom(true);
     }, []);
+    const onClose = useCallback(() => {
+        setShowImagesZomm(false);
+    }, []);
+
     if (images.length === 1) {
         return (
             <>
                 <img role="presentation" src={images[0].src} alt={images[0].src} onClick={onZoom} />
+                {showImagesZoom && <ImagesZoom images={images} onClose={onClose} />}
             </>
         );
     }
@@ -20,6 +27,7 @@ const PostImages = ({ images }) => {
             <>
                 <img role="presentation" style={{ width: '50%', display: 'inline-block' }} src={images[0].src} alt={images[0].src} onClick={onZoom} />
                 <img role="presentation" style={{ width: '50%', display: 'inline-block' }} src={images[1].src} alt={images[1].src} onClick={onZoom} />
+                {showImagesZoom && <ImagesZoom images={images} onClose={onClose} />}
             </>
         );
     }
@@ -38,6 +46,7 @@ const PostImages = ({ images }) => {
                     개의 사진 더보기
                 </div>
             </div>
+            {showImagesZoom && <ImagesZoom images={images} onClose={onClose} />}
         </>
     )
 };
