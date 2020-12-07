73bee9c17bbf9f4279df89e294c83e1cbaad31d9
guswns951230 <rkdguswns0803@gmail.com>
2020-12-07 16:37:11 +0900

    saga 쪼개고 reducer와 연결하기

--- front/components/LoginForm.js
+++ front/components/LoginForm.js
@@ -2,10 +2,10 @@ import React, { useCallback } from 'react';
 import { Form, Input, Button } from 'antd';
 import Link from 'next/link';
 import styled from 'styled-components';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import useInput from '../hooks/useInput';
-import { loginAction } from '../reducers/user';
+import { loginRequestAction } from '../reducers/user';
 
 const FormWrapper = styled(Form)`
     padding: 10px;
@@ -16,12 +16,13 @@ const ButtonWrapper = styled.div`
 
 const LoginForm = () => {
     const dispatch = useDispatch();
+    const { isLoggingIn } = useSelector((state) => state.user);
     const [id, onChangeId] = useInput('');
     const [password, onChangePassword] = useInput('');
 
     const onSubmitForm = useCallback(() => {
         console.log(id, password);
-        dispatch(loginAction({ id, password }));
+        dispatch(loginRequestAction({ id, password }));
     }, [id, password]);
 
     return (
@@ -43,7 +44,7 @@ const LoginForm = () => {
                 />
             </div>
             <ButtonWrapper>
-                <Button type="primary" htmlType="submit" loading={false}>로그인</Button>
+                <Button type="primary" htmlType="submit" loading={isLoggingIn}>로그인</Button>
                 <Link href="/signup"><a><Button style={{ marginLeft: 5 }}>회원가입</Button></a></Link>
             </ButtonWrapper>
         </FormWrapper>
