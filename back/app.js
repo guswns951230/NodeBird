20fcbfbf5fdb717940583d2fe29cf20905d2ce62
guswns951230 <rkdguswns0803@gmail.com>
2020-12-18 01:17:41 +0900

    node로 server 구동하기

---
+++ back/app.js
@@ -0,0 +1,12 @@
+const http = require('http');
+const server = http.createServer((req, res) => {
+    console.log(req.url, req.method);
+    res.write('Hello node1');
+    res.write('Hello node2');
+    res.write('Hello node3');
+    res.write('Hello node4');
+    res.end('Hello node5');
+});
+server.listen(3065, () => {
+    console.log('서버 실행중');
+});
