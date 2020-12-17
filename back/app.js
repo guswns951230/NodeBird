b6234795919a40785b4b933d397c371b57423307
guswns951230 <rkdguswns0803@gmail.com>
2020-12-18 01:34:52 +0900

    express로 routing하기

--- back/app.js
+++ back/app.js
@@ -1,12 +1,32 @@
-const http = require('http');
-const server = http.createServer((req, res) => {
-    console.log(req.url, req.method);
-    res.write('Hello node1');
-    res.write('Hello node2');
-    res.write('Hello node3');
-    res.write('Hello node4');
-    res.end('Hello node5');
+const express = require('express');
+const { Http2ServerRequest } = require('http2');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('hello express');
 });
-server.listen(3065, () => {
-    console.log('서버 실행중');
+
+app.get('/api', (req, res) => {
+    res.send('hello api');
+});
+
+app.get('/api/posts', (req, res) => {
+    res.json([
+        { id: 1, content: 'hello' },
+        { id: 2, content: 'hello2' },
+        { id: 3, content: 'hello3' },
+    ]);
+});
+
+app.post('/api/post', (req, res) => {
+    res.json({ id: 1, content: 'hello' });
+});
+
+app.delete('/api/post', (req, res) => {
+    res.json({ id: 1 });
+});
+
+app.listen(3065, () => {
+    console.log('서버 실행 중');
 });
