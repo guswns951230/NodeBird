236e0bbda273510c61ccab3b1048d41cccb130cf
guswns951230 <rkdguswns0803@gmail.com>
2020-12-18 01:43:05 +0900

    express router 분리하기

--- back/app.js
+++ back/app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { Http2ServerRequest } = require('http2');
+const postRouter = require('./routes/post');
 
 const app = express();
 
@@ -7,11 +7,11 @@ app.get('/', (req, res) => {
     res.send('hello express');
 });
 
-app.get('/api', (req, res) => {
+app.get('/', (req, res) => {
     res.send('hello api');
 });
 
-app.get('/api/posts', (req, res) => {
+app.get('/posts', (req, res) => {
     res.json([
         { id: 1, content: 'hello' },
         { id: 2, content: 'hello2' },
@@ -19,13 +19,7 @@ app.get('/api/posts', (req, res) => {
     ]);
 });
 
-app.post('/api/post', (req, res) => {
-    res.json({ id: 1, content: 'hello' });
-});
-
-app.delete('/api/post', (req, res) => {
-    res.json({ id: 1 });
-});
+app.use('/post', postRouter);
 
 app.listen(3065, () => {
     console.log('서버 실행 중');
