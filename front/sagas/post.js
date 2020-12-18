254dc89609d364932692fda6e11aca592dab9ede
guswns951230 <rkdguswns0803@gmail.com>
2020-12-19 04:18:33 +0900

    게시글, 댓글 작성하기

--- front/sagas/post.js
+++ front/sagas/post.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import shortId from 'shortid';
-import { delay, put, takeLatest, all, fork, throttle } from 'redux-saga/effects';
+import { delay, put, takeLatest, all, fork, throttle, call } from 'redux-saga/effects';
 import {
     ADD_POST_REQUEST, ADD_POST_SUCCESS, ADD_POST_FAILURE,
     LOAD_POSTS_REQUEST, LOAD_POSTS_SUCCESS, LOAD_POSTS_FAILURE,
@@ -31,23 +31,18 @@ function* loadPosts(action) {
 }
 
 function addPostAPI(data) {
-    return axios.post('/api/post', data)
+    return axios.post('/post', { content: data });
 }
 function* addPost(action) {
     try {
-        // const result = yield call(addPostAPI, action.data);
-        yield delay(1000);
-        const id = shortId.generate();
+        const result = yield call(addPostAPI, action.data);
         yield put({
             type: ADD_POST_SUCCESS,
-            data: {
-                id,
-                content: action.data,
-            },
+            data: result.data,
         });
         yield put({
             type: ADD_POST_TO_ME,
-            data: id,
+            data: result.data.id,
         });
     } catch (err) {
         yield put({
@@ -82,15 +77,14 @@ function* removePost(action) {
 }
 
 function addCommentAPI(data) {
-    return axios.post(`/api/post/${data.postId}/comment`, data)
+    return axios.post(`/post/${data.postId}/comment`, data) //POST /post/1/comment
 }
 function* addComment(action) {
     try {
-        // const result = yield call(addCommentAPI, action.data);
-        yield delay(1000);
+        const result = yield call(addCommentAPI, action.data);
         yield put({
             type: ADD_COMMENT_SUCCESS,
-            data: action.data,
+            data: result.data,
         });
     } catch (err) {
         yield put({
