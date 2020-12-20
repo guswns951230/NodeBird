65a3f9bd46a985288f3ee0511587834339dd10f9
guswns951230 <rkdguswns0803@gmail.com>
2020-12-21 03:07:04 +0900

    Follow / Unfollow

--- back/routes/user.js
+++ back/routes/user.js
@@ -118,4 +118,74 @@ router.patch('/nickname', isLoggedIn, async (req, res, next) => {
     }
 });
 
+router.patch('/:userId/follow', isLoggedIn, async (req, res, next) => { // PATCH /user/1/follow
+    try {
+        const user = await User.findOne({ where: { id: req.params.userId } });
+        if (!user) {
+            res.status(403).send('존재하지 않는 사용자 입니다.');
+        }
+        await user.addFollowers(req.user.id);
+        res.status(200).json({ UserId: parseInt(req.params.userId, 10) });
+    } catch (error) {
+        console.error(error);
+        next(error);
+    }
+});
+
+router.delete('/:userId/follow', isLoggedIn, async (req, res, next) => { // DELETE /user/1/follow
+    try {
+        const user = await User.findOne({ where: { id: req.params.userId } });
+        if (!user) {
+            res.status(403).send('존재하지 않는 사용자 입니다.');
+        }
+        await user.removeFollowers(req.user.id);
+        res.status(200).json({ UserId: parseInt(req.params.userId, 10) });
+    } catch (error) {
+        console.error(error);
+        next(error);
+    }
+});
+
+router.get('/followers', isLoggedIn, async (req, res, next) => { // GET /user/followers
+    try {
+        const user = await User.findOne({ where: { id: req.user.id } });
+        if (!user) {
+            res.status(403).send('존재하지 않는 사용자 입니다.');
+        }
+        const followers = await user.getFollowers();
+        res.status(200).json(followers);
+    } catch (error) {
+        console.error(error);
+        next(error);
+    }
+});
+
+router.get('/followings', isLoggedIn, async (req, res, next) => { // GET /user/followings
+    try {
+        const user = await User.findOne({ where: { id: req.user.id } });
+        if (!user) {
+            res.status(403).send('존재하지 않는 사용자 입니다.');
+        }
+        const followings = await user.getFollowings();
+        res.status(200).json(followings);
+    } catch (error) {
+        console.error(error);
+        next(error);
+    }
+});
+
+router.delete('/follower/:userId', isLoggedIn, async (req, res, next) => { // DELETE /user/follower/2
+    try {
+        const user = await User.findOne({ where: { id: req.params.userId } });
+        if (!user) {
+            res.status(403).send('차단할 수 없는 사용자 입니다.');
+        }
+        await user.removeFollowings(req.user.id);
+        res.status(200).json({ UserId: parseInt(req.params.userId, 10) });
+    } catch (error) {
+        console.error(error);
+        next(error);
+    }
+});
+
 module.exports = router;
--- front/sagas/user.js
+++ front/sagas/user.js
@@ -1,6 +1,9 @@
 import axios from 'axios';
-import { delay, put, takeLatest, all, fork, call } from 'redux-saga/effects';
+import { put, takeLatest, all, fork, call } from 'redux-saga/effects';
 import {
+    REMOVE_FOLLOWER_REQUEST, REMOVE_FOLLOWER_SUCCESS, REMOVE_FOLLOWER_FAILURE,
+    LOAD_FOLLOWERS_REQUEST, LOAD_FOLLOWERS_SUCCESS, LOAD_FOLLOWERS_FAILURE,
+    LOAD_FOLLOWINGS_REQUEST, LOAD_FOLLOWINGS_SUCCESS, LOAD_FOLLOWINGS_FAILURE,
     CHANGE_NICKNAME_REQUEST, CHANGE_NICKNAME_SUCCESS, CHANGE_NICKNAME_FAILURE,
     LOAD_MY_INFO_REQUEST, LOAD_MY_INFO_SUCCESS, LOAD_MY_INFO_FAILURE,
     FOLLOW_REQUEST, FOLLOW_SUCCESS, FOLLOW_FAILURE,
@@ -9,6 +12,63 @@ import {
     LOG_OUT_REQUEST, LOG_OUT_SUCCESS, LOG_OUT_FAILURE,
     SIGN_UP_REQUEST, SIGN_UP_SUCCESS, SIGN_UP_FAILURE,
 } from '../reducers/user';
+
+function removeFollowerAPI(data) {
+    return axios.delete(`/user/follower/${data}`);
+}
+
+function* removeFollower(action) {
+    try {
+        const result = yield call(removeFollowerAPI, action.data);
+        yield put({
+            type: REMOVE_FOLLOWER_SUCCESS,
+            data: result.data,
+        });
+    } catch (err) {
+        yield put({
+            type: REMOVE_FOLLOWER_FAILURE,
+            error: err.response.data,
+        });
+    }
+}
+
+function loadFollowersAPI(data) {
+    return axios.get('/user/followers', data);
+}
+
+function* loadFollowers(action) {
+    try {
+        const result = yield call(loadFollowersAPI, action.data);
+        yield put({
+            type: LOAD_FOLLOWERS_SUCCESS,
+            data: result.data,
+        });
+    } catch (err) {
+        yield put({
+            type: LOAD_FOLLOWERS_FAILURE,
+            error: err.response.data,
+        });
+    }
+}
+
+function loadFollowingsAPI(data) {
+    return axios.get('/user/followings', data);
+}
+
+function* loadFollowings(action) {
+    try {
+        const result = yield call(loadFollowingsAPI, action.data);
+        yield put({
+            type: LOAD_FOLLOWINGS_SUCCESS,
+            data: result.data,
+        });
+    } catch (err) {
+        yield put({
+            type: LOAD_FOLLOWINGS_FAILURE,
+            error: err.response.data,
+        });
+    }
+}
 
 function changeNicknameAPI(data) {
     return axios.patch('/user/nickname', { nickname: data });
@@ -104,17 +164,16 @@ function* signUp(action) {
     }
 }
 
-function followAPI() {
-    return axios.post('/api/follow')
+function followAPI(data) {
+    return axios.patch(`/user/${data}/follow`);
 }
 
 function* follow(action) {
     try {
-        // const result = yield call(followAPI);
-        yield delay(1000);
+        const result = yield call(followAPI, action.data);
         yield put({
             type: FOLLOW_SUCCESS,
-            data: action.data,
+            data: result.data,
         });
     } catch (err) {
         yield put({
@@ -124,17 +183,16 @@ function* follow(action) {
     }
 }
 
-function unfollowAPI() {
-    return axios.post('/api/unfollow')
+function unfollowAPI(data) {
+    return axios.delete(`/user/${data}/follow`)
 }
 
 function* unfollow(action) {
     try {
-        // const result = yield call(unfollowAPI);
-        yield delay(1000);
+        const result = yield call(unfollowAPI, action.data);
         yield put({
             type: UNFOLLOW_SUCCESS,
-            data: action.data,
+            data: result.data,
         });
     } catch (err) {
         yield put({
@@ -142,6 +200,18 @@ function* unfollow(action) {
             error: err.response.data,
         });
     }
+}
+
+function* watchRemoveFollower() {
+    yield takeLatest(REMOVE_FOLLOWER_REQUEST, removeFollower);
+}
+
+function* watchLoadFollowings() {
+    yield takeLatest(LOAD_FOLLOWINGS_REQUEST, loadFollowings);
+}
+
+function* watchLoadFollowers() {
+    yield takeLatest(LOAD_FOLLOWERS_REQUEST, loadFollowers);
 }
 
 function* watchChangeNickname() {
@@ -174,6 +244,9 @@ function* watchSignUp() {
 
 export default function* userSaga() {
     yield all([
+        fork(watchRemoveFollower),
+        fork(watchLoadFollowings),
+        fork(watchLoadFollowers),
         fork(watchChangeNickname),
         fork(watchLoadMyInfo),
         fork(watchFollow),
